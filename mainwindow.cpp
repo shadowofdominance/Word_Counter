b60ec761c644b2ed3e23690efe9aa9e3c916e2a0
Gourav Sapaliga <130655508+shadowofdominance@users.noreply.github.com>
2026-03-27 20:19:34 +0530

    Added Comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,17 +16,22 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_textEdit_textChanged()
 {
+    //Fetching the text from the textEdit
     QString text = ui->textEdit->toPlainText();
 
+    //Counting the characters
     int characters = text.length();
 
+    //Counting the words by splitting the whole text into words using spaces and next line characters!
     QStringList words = text.split(QRegularExpression("\\s+"), Qt::SkipEmptyParts);
     int wordCount = words.count();
 
+    /* Counting the sentences through using regular expressions to check where in the text we have these ".!?"
+    and split them into sentences. */
     QStringList sentences = text.split(QRegularExpression("[.!?]+"), Qt::SkipEmptyParts);
     int sentenceCount = sentences.count();
 
-
+    //Updating the UI Labels
     ui->WordsLabel->setText("Words: " + QString::number(wordCount));
     ui->CharactersLabel->setText("Characters " + QString::number(characters));
     ui->SentencesLabel->setText("Sentences: " + QString::number(sentenceCount));
@@ -37,6 +42,7 @@ void MainWindow::on_ClearButton_clicked()
     ui->textEdit->clear();
 }
 
+//Searching a word and calculating the frequency of that word!
 void MainWindow::on_SearchBoxLineEdit_returnPressed()
 {
     QString text = ui->textEdit->toPlainText();
@@ -49,6 +55,7 @@ void MainWindow::on_SearchBoxLineEdit_returnPressed()
 
     int count = 0;
 
+    //Seacrhing the word occurences
     for(const QString &word : words){
         if(word == searchWord){
             count++;
@@ -58,7 +65,7 @@ void MainWindow::on_SearchBoxLineEdit_returnPressed()
     ui->FrequencyLabel->setText("Occurrences: " + QString::number(count));
 }
 
-
+//Convert selected text into lowercase
 void MainWindow::on_LowerCaseButton_clicked()
 {
     QTextCursor cursor = ui->textEdit->textCursor();
@@ -70,7 +77,7 @@ void MainWindow::on_LowerCaseButton_clicked()
     }
 }
 
-
+//Convert selected text into UpperCase
 void MainWindow::on_UpperCaseButton_clicked()
 {
     QTextCursor cursor = ui->textEdit->textCursor();
@@ -81,4 +88,3 @@ void MainWindow::on_UpperCaseButton_clicked()
         cursor.insertText(text);
     }
 }
-
